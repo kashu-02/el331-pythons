897d1a5f9bb73503aac549e1a114bd6f1384efd6
yuorei <108039575+yuorei@users.noreply.github.com>
2024-05-25 19:45:05 +0900

    chore: Add login functionality and user settings management

--- data_set_management_tab/main.py
+++ data_set_management_tab/main.py
@@ -1,9 +1,14 @@
 import sqlite3
+from datetime import datetime
 
 # FILE.dbの中にいろいろなデータベースがある
 
 
 class TextFileCRUD:
+    name = ""
+    case_number = 0
+    case_name = ""
+    isHistory = False
     def __init__(self, dbname="FILE.db"):
         self.dbname = dbname
         #   | id | tittle | text |
@@ -16,6 +21,12 @@ class TextFileCRUD:
             cur = conn.cursor()
             cur.execute(
                 "CREATE TABLE IF NOT EXISTS texts(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,text TEXT)",
+            )
+            # cur.execute(
+            #     "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT)",
+            # )
+            cur.execute(
+                "CREATE TABLE IF NOT EXISTS settings(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,case_number INTEGER,case_name TEXT,date TEXT)",
             )
             conn.commit()
             conn.close()
@@ -61,24 +72,20 @@ class TextFileCRUD:
             print("An error occurred:", e)
 
     def search_data(self, search_term, target):
+        if self.isHistory:
+            search_term = str(self.case_number)
         try:
             conn = sqlite3.connect(self.dbname)
             cur = conn.cursor()
-            #                "SELECT title, text FROM texts WHERE title = ? OR id = ? LIKE ?",
-            #               (search_term, search_term, "%" + target + "%"),
             cur.execute(
-                "SELECT title, text FROM texts WHERE title = ? OR id = ? LIKE ?",
-                (search_term, search_term, "%" + target + "%"),
+                "SELECT title, text FROM texts WHERE title = ? OR id = ?",
+                (search_term, search_term),
             )
-            # rows = [[title, text], [title, text],...]
             rows = cur.fetchall()
-            # print(rows)
 
-            # print(rows[0])
             title, sentence = rows[0]
             row = list(sentence.split())
-            # print("attention")
-            # print(row)
+
             result = []
             l = len(row)
             #            print(row[3])
@@ -89,6 +96,13 @@ class TextFileCRUD:
                     result.append(row[0 : i + 6])
                 elif row[i] == target and 0 <= (i - 5) and l < (i + 5):
                     result.append(row[i - 5 : l])
+            if self.name == "":
+                name = input("Please input User Name: ")
+                case_number = int(input("Please input Case Number: "))
+                case_name = input("Please input Case Name: ")
+                now = datetime.now()
+                date = now.strftime("%d %b %Y")
+                self.insert_setting(name, case_number, case_name, date)
             return title, result
         except Exception as e:
             print("An error occurred:", e)
@@ -121,13 +135,48 @@ class TextFileCRUD:
         except Exception as e:
             print("An error occurred:", e)
 
+    def read_setting(self, name):
+        try:
+            conn = sqlite3.connect(self.dbname)
+            cur = conn.cursor()
+            cur.execute(
+                "SELECT id, name, case_number, case_name, date FROM settings WHERE name = ?",
+                (name,),
+            )
+
+            rows = cur.fetchall()
+
+            conn.close()
+
+            formatted_rows = [(f"ID: {row[0]}, Name: {row[1]}, Case Number: {row[2]}, Case Name: {row[3]}, Date: {row[4]}") for row in rows]
+            self.name = rows[0][1]
+            self.case_number = int(rows[0][2])
+            self.case_name = rows[0][3]
+            self.isHistory = True
+            return formatted_rows
+        except Exception as e:
+            print("An error occurred:", e)
+
+    def insert_setting(self, name, case_number, case_name, date):
+        try:
+            conn = sqlite3.connect(self.dbname)
+            cur = conn.cursor()
+            cur.execute("INSERT INTO settings(name, case_number, case_name, date) VALUES (?, ?, ?, ?)", (name, case_number, case_name, date))
+            conn.commit()
+            conn.close()
+            print("Data has been added.")
+            self.isHistory = True
+        except Exception as e:
+            print("An error occurred:", e)
+
+
 
 def main():
     handler = TextFileCRUD(dbname="FILE.db")
     handler.create_table()
     while True:
         operation = input(
-            "Please input the operation (C: Create, R: Read, U: Update, D: Delete, Q: Quit, S: SearchWord): ",
+            "Please input the operation (C: Create, R: Read, U: Update, D: Delete, Q: Quit, S: SearchWord, L: Login): "
         )
 
         if operation.upper() == "C":
@@ -160,6 +209,14 @@ def main():
 
         elif operation.upper() == "Q":
             break
+
+        elif operation.upper() == "L":
+            name = input("Please input User Name. ")
+            if name == "":
+                continue
+            result = handler.read_setting(name)
+            print(result)
+            print("Login Success!")
 
         else:
             print("Invalid operation.")
